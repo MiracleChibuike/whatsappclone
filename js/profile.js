639818862c87e355c0af2843ffb61bfb0dc5512e
MiracleChibuike <miraclechibuike27@gmail.com>
2023-12-13 22:09:47 +0100

    time activated

--- js/profile.js
+++ js/profile.js
@@ -4,7 +4,16 @@ let inputBox = document.getElementById("typeMessage");
 
 let sendmsg = document.querySelector("#send");
 
-var seeViewChat = document.querySelector(".chatDisplay")
+var seeViewChat = document.querySelector(".chatDisplay");
+let showCurrentTime = document.querySelector(".getTimeCurrent");
+console.log(showCurrentTime)
+var update = new Date();
+// usersLastLogin.innerHTML = "hello"
+var date = new Date();
+var current_date = date.getDate() + "/ " + (date.getMonth()+1) + " / " + date.getFullYear();
+var curent_time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+var date_time = update.toDateString();
+showCurrentTime.textContent = date_time;
 // sendmsg.addEventListener("click", function () {
 //     alert(inputBox.value)
 // })
@@ -27,15 +36,19 @@ sendmsg.addEventListener("click", function (params) {
         alert("empty box cannot send")
     }else{
         seeViewChat.innerHTML += `
-        <div class="messagecontent">
-                <p class="va">
-                    ${inputBox.value}
-                </p>
-            </div>
+         <div class="messagecontent">
+            <p class="va">
+            ${inputBox.value}
+                <small id="spanLocalTime">${showCurrentTime.textContent = date_time}</small>
+
+            </p>
+        </div>
         
         `
         inputBox.value = " "
     }
 })
+
+
 
  
